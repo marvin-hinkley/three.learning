2de1b226a22cf7db8d3a3380565a17628c522f46
marvinhinkley <hinkley.marvin@gmail.com>
2015-04-11 14:15:52 -0700

    updated server file

--- server.js
+++ server.js
@@ -4,20 +4,16 @@ var express         = require('express'),
     morgan          = require('morgan'),
     bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
-    mongooose       = require('mongoose'),
-    app             = express(),
-    io              = require('socket.io');
+    app             = express()
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var server = http.createServer(app).listen(process.env.PORT || 8080, function(){}),
-    socketServer = io.listen(server);
+var server = http.createServer(app).listen(process.env.PORT || 8080, function(){})
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: path.join(__dirname, '/public') });
-//    res.sendFile('dist/index.html');
+    res.sendFile('index.html', { root: path.join(__dirname, '/client') });
 });
